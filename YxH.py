aaf778a4f463fef014441b06459a835d90344012
Alpha <124826956+imr-16@users.noreply.github.com>
2024-03-24 18:38:30 +0530

    Update YxH.py

--- YxH.py
+++ YxH.py
@@ -2,9 +2,14 @@ from pyrogram import Client, idle
 from config import *
 
 YxH = Client(
-        ":YxH:",
-        api_id=API_ID,
-        api_hash=API_HASH,
-        bot_token=BOT_TOKEN,
-        plugins=dict(root="YxH/Plugins")
-    )
+    ":YxH:",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="YxH/Plugins")
+)
+
+def __init__():
+    YxH.start()
+    print("Bot Started.")
+    idle()
