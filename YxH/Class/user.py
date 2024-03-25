fb3ca056c5d6f6c7fbc0ab4cf94d5f6ebbb877d5
Alpha <124826956+imr-16@users.noreply.github.com>
2024-03-25 14:58:08 +0530

    Create user.py

---
+++ YxH/Class/user.py
@@ -0,0 +1,9 @@
+class User:
+  def __init__(user):
+    self.user = user
+    self.crystals = 0
+    self.gems = 0
+    self.coins = 0
+    self.collection = {}
+    self.profile_picture = None
+    self.active_bot_id = 0
