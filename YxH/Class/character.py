d88e341bffb4b8b5bd0ef9f804b096beb42f52e2
Alpha <124826956+imr-16@users.noreply.github.com>
2024-03-25 21:00:27 +0530

    Create character.py

---
+++ YxH/Class/character.py
@@ -0,0 +1,22 @@
+import pickle
+import random
+from YxH.Database import db
+
+class Character:
+  def __init__(self, id, image, name, anime, rarity, price=0):
+    self.id = id
+    self.image = image
+    self.name = name
+    self.anime = anime
+    self.rarity = rarity
+    if price == 0:
+      self.price = random.choice(list(range(30000, 60001)))
+    else:
+      self.price = price
+
+  async def add(self):
+    await db.characters.update_one(
+      {'id': self.id},
+      {'$set': {'info': pickle.dumps(self)}},
+      upsert=True
+    )
