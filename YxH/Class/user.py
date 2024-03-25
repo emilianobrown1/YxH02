41cab93d6e7f9d4241b4ed6a038f1ba15904e0e9
Alpha <124826956+imr-16@users.noreply.github.com>
2024-03-25 15:04:36 +0530

    Update user.py

--- YxH/Class/user.py
+++ YxH/Class/user.py
@@ -1,3 +1,5 @@
+from YxH.Database import db
+
 class User:
   def __init__(user):
     self.user = user
@@ -7,3 +9,18 @@ class User:
     self.collection = {}
     self.profile_picture = None
     self.active_bot_id = 0
+
+  async def update_crystals(self, value):
+    await db.crystals.update_one(
+      {'user_id': self.user.id},
+      {'$set': {'crystals': value}},
+      upsert=True
+    )
+
+  async def get_crystals(self):
+    x = await db.crystals.find_one(
+      {'user_id': user_id}
+    )
+    if not x:
+      return 0
+    return x['crystals']
