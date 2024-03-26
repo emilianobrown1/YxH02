5f8f9db3dbb1a92cc3d4c59088019953fea55bf1
Alpha <124826956+imr-16@users.noreply.github.com>
2024-03-26 19:44:50 +0530

    Update markups.py

--- YxH/Utils/markups.py
+++ YxH/Utils/markups.py
@@ -1,7 +1,18 @@
 from pyrogram.types import InlineKeyboardMarkup as ikm, InlineKeyboardButton as ikb
-from info import bot_info
+from . import bot_info
+from config import SUPPORT_GROUP, SUPPORT_CHANNEL
 
-start_markup = ikm(
-  [
+async def start_markup():
+  info = await bot_info()
+  start_markup = ikm(
     [
-      
+      [
+        ikb("Add me to your group", url=f"https://t.me/{info.username}?startgroup=True")
+      ],
+      [
+        ikb("Group", url=f"https://t.me/{SUPPORT_GROUP}"),
+        ikb("Channel", url=f"https://t.me/{SUPPORT_CHANNEL}")
+      ]
+    ]
+  )
+  return start_markup
