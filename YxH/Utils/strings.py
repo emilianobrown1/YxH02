662f7f31a3f3dc241bb2444910a00cdea31f53eb
Alpha <124826956+imr-16@users.noreply.github.com>
2024-03-28 18:40:06 +0530

    Update strings.py

--- YxH/Utils/strings.py
+++ YxH/Utils/strings.py
@@ -1,1 +1,4 @@
 start_text = "Hey buddy {}, I'm a game bot to entertain users, Add me to your group and checkout my commands as well."
+block_text = "You have been blocked by admins."
+negate_private_text = "This command doesn't work in private chats."
+negate_group_text = "This command doesn't work in groups."
