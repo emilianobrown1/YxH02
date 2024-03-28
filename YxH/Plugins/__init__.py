5cbe749965c37c965158a53aa7156dadde2d0c33
Alpha <124826956+imr-16@users.noreply.github.com>
2024-03-28 18:41:51 +0530

    Update __init__.py

--- YxH/Plugins/__init__.py
+++ YxH/Plugins/__init__.py
@@ -2,3 +2,4 @@ from . import bot_info
 from .Utils.datetime import get_date, get_week
 from .Database.users import get_user
 from .Database.characters import get_character
+from .universal_decorator import YxH
