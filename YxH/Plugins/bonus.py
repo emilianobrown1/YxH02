5858db2c7e180b24caba0f327f5709fb8a3fc3e5
Alpha <124826956+imr-16@users.noreply.github.com>
2024-03-28 18:52:06 +0530

    Update bonus.py

--- YxH/Plugins/bonus.py
+++ YxH/Plugins/bonus.py
@@ -1,1 +1,20 @@
+from pyrogram import Client, filters
+from . import YxH, get_date, get_week
 
+@Client.on_message(filters.command("claim"))
+@YxH()
+async def claim(_, m, user):
+  date, week = get_date(), get_week()
+  l = [date, week]
+  if user.bonus == l:
+    return await m.reply("You have already claimed your bonus.")
+  if user.bonus[1] != week:
+    await m.reply("Weekly and Daily Bonus has been claimed, 550000")
+    user.gems += 550000
+    user.bonus = l
+    return await user.update()
+  if user.bonus[0] != date:
+    await m.reply("Daily Bonus has been claimed, 50000")
+    user.gems += 50000
+    user.bonus = l
+    return await user.update()
