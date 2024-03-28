5f92ae3032e12ff340c6c28b1d4785554e81eb4c
Alpha <124826956+imr-16@users.noreply.github.com>
2024-03-28 18:19:18 +0530

    Update characters.py

--- YxH/Database/characters.py
+++ YxH/Database/characters.py
@@ -1,9 +1,10 @@
 from . import db
+import pickle
 
 db = db.characters
 
 async def get_character(id):
   x = await db.find_one({'id': id})
   if x:
-    return x['info']
-  return {}
+    return pickle.loads(x['info'])
+  return None
