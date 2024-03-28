7b339e9be1e4250d45087786f361d14463bb7349
Alpha <124826956+imr-16@users.noreply.github.com>
2024-03-28 23:01:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_HASH = getenv("API_HASH", "83d8e423197251216303abfcbed9e820")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "7006726437:AAEUqcQLuKiUbiF_gM26TrEl47abHPoyGjQ")
   
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://phil:phil@cluster0.pl9hrrz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://phil:phil@cluster0.vvs2pmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 SUDO_USERS = getenv("SUDO_USERS", "5903688119 1965472544 5910101206")
 OWNER_ID = int(getenv("OWNER_ID", "5903688119"))
