8ebd0168fcec32f6d2ec7cafcef5982ea9ffca3b
Alpha <124826956+imr-16@users.noreply.github.com>
2024-03-30 20:16:54 +0530

    Update templates.py

--- YxH/Utils/templates.py
+++ YxH/Utils/templates.py
@@ -6,5 +6,6 @@ async def get_image_and_caption(id):
   cap += f"Name: {c.name}\n"
   cap += f"{c.category}: {c.category_name}\n"
   cap += f"Price: {c.price}\n"
+  cap += f"Rarity: {c.rarity}\n"
   cap += f"ID: {c.id}"
   return c.image, cap
