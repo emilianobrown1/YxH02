b7bfa8c0d8fb56e3240e7807216cbfbfa52667a9
Alpha <124826956+imr-16@users.noreply.github.com>
2024-03-30 23:06:53 +0530

    Update templates.py

--- YxH/Utils/templates.py
+++ YxH/Utils/templates.py
@@ -1,11 +1,11 @@
-from ..Database.characters import get_character
+from ..Database.characters import get_anime_character
 
-async def get_image_and_caption(id):
-  c = await get_character(id)
+async def get_anime_image_and_caption(id):
+  c = await get_anime_character(id)
   cap = ""
-  cap += f"Name: {c.name}\n"
-  cap += f"{c.category}: {c.category_name}\n"
-  cap += f"Price: {c.price}\n"
-  cap += f"Rarity: {c.rarity}\n"
+  cap += f"Name: {c.name}\n\n"
+  cap += f"Anime: {c.anime}\n\n"
+  cap += f"Price: {c.price}\n\n"
+  cap += f"Rarity: {c.rarity}\n\n"
   cap += f"ID: {c.id}"
   return c.image, cap
