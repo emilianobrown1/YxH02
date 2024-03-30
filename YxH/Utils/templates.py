3b6b8686b8d58e3850b300fde1d46208e085a992
Alpha <124826956+imr-16@users.noreply.github.com>
2024-03-30 20:14:46 +0530

    Create templates.py

---
+++ YxH/Utils/templates.py
@@ -0,0 +1,10 @@
+from ..Database.characters import get_character
+
+async def get_image_and_caption(id):
+  c = await get_character(id)
+  cap = ""
+  cap += f"Name: {c.name}\n"
+  cap += f"{c.category}: {c.category_name}\n"
+  cap += f"Price: {c.price}\n"
+  cap += f"ID: {c.id}"
+  return c.image, cap
