6cc0fd2c335f4c191cb7cdf3adb3559b4bc83be4
Alpha <124826956+imr-16@users.noreply.github.com>
2024-03-30 23:16:23 +0530

    Update templates.py

--- YxH/Utils/templates.py
+++ YxH/Utils/templates.py
@@ -1,6 +1,6 @@
 from ..Database.characters import get_anime_character
 
-async def get_anime_image_and_caption(id):
+async def get_anime_image_and_caption(id: int) -> str:
   c = await get_anime_character(id)
   cap = "🎭 𝙉𝘼𝙈𝙀 : {}\n\n🎖𝘼𝙉𝙄𝙈𝙀 : {}\n\n💰 𝙋𝙍𝙄𝘾𝙀 : {} 💎\n\n🆔 : {}".format(c.name, c.anime, c.price, c.id)
   return cap
