8764095afd1f2f90b1481988f2a4a35f44cf9239
Alpha <124826956+imr-16@users.noreply.github.com>
2024-03-30 20:36:39 +0530

    Update load_attr.py

--- YxH/load_attr.py
+++ YxH/load_attr.py
@@ -1,17 +1,14 @@
 from .Database.users import get_user
 from .Class.user import User
 
-attr = []
-
-def init_attr_list():
-  global attr
-  if not attr:
-    d = dir(User)
-    for x in d:
-      if not callable(x):
-        attr.append(x)
+attr = [x for x in dir(User) if not callable(x)]
+ex_user = User("YxH")
 
 async def load_attr(user_id: int):
-  ex_user = User()
   user = await get_user(user_id)
-  user_attr = 
+  user_attr = [x for x in dir(user) if not callable(x)]
+  for x in attr:
+    if hasattr(user, x):
+      continue
+    setattr(user, x, getattr(ex_user, x))
+  await user.update()
