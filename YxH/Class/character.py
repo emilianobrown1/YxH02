2455d21c765b78d9c68205be8a0f11b53b342895
Alpha <124826956+imr-16@users.noreply.github.com>
2024-03-30 23:00:00 +0530

    Update character.py

--- YxH/Class/character.py
+++ YxH/Class/character.py
@@ -2,13 +2,12 @@ import pickle
 import random
 from YxH.Database import db
 
-class Character:
-  def __init__(self, id, image, name, category, category_name, rarity, price=0):
+class AnimeCharacter:
+  def __init__(self, id, image, name, anime, rarity, price=0):
     self.id = id
     self.image = image
     self.name = name
-    self.category = category
-    self.category_name = category_name
+    self.anime = anime
     self.rarity = rarity
     if price == 0:
       self.price = random.choice(list(range(30000, 60001)))
@@ -16,7 +15,24 @@ class Character:
       self.price = price
 
   async def add(self):
-    await db.characters.update_one(
+    await db.anime_characters.update_one(
+      {'id': self.id},
+      {'$set': {'info': pickle.dumps(self)}},
+      upsert=True
+    )
+    
+class YaoiYuriCharacter:
+  def __init__(self, id, image, name, price=0):
+    self.id = id
+    self.image = image
+    self.name = name
+    if price == 0:
+      self.price = random.choice(list(range(30000, 60001)))
+    else:
+      self.price = price
+
+  async def add(self):
+    await db.yaoiyuri_characters.update_one(
       {'id': self.id},
       {'$set': {'info': pickle.dumps(self)}},
       upsert=True
