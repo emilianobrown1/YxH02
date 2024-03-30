028123618648808c82581d8c48d53c83cddf4e3e
Alpha <124826956+imr-16@users.noreply.github.com>
2024-03-30 23:10:26 +0530

    Update characters.py

--- YxH/Database/characters.py
+++ YxH/Database/characters.py
@@ -1,16 +1,14 @@
 from . import db
 import pickle
 
-db = db.characters
-
-async def get_character(id):
-  x = await db.find_one({'id': id})
+async def get_anime_character(id):
+  x = await db.anime_characters.find_one({'id': id})
   if x:
     return pickle.loads(x['info'])
   return None
 
-async def characters_count():
-  x = db.find()
+async def anime_characters_count():
+  x = db.anime_characters.find()
   if not x:
     return 0
   return len(await x.to_list(length=None))
