50d427be78d1f759233df067b07c9eb90b3ae267
Alpha <124826956+imr-16@users.noreply.github.com>
2024-03-31 11:54:26 +0530

    Create eval.py

---
+++ YxH/Plugins/eval.py
@@ -0,0 +1,86 @@
+import os
+import re
+import asyncio
+import subprocess
+import sys
+import traceback
+from inspect import getfullargspec
+from io import StringIO
+from time import time
+from ..Database import db
+from pyrogram import filters
+from pyrogram import Client, filters
+from config import OWNER_ID as OWNER
+
+SUDOERS = filters.user(OWNER)
+
+async def aexec(code, client, message):
+    exec(
+        "async def __aexec(client, message): "
+        + "".join(f"\n {a}" for a in code.split("\n"))
+    )
+    return await locals()["__aexec"](client, message)
+
+
+async def edit_or_reply(msg: Message, **kwargs):
+    func = msg.edit_text if msg.from_user.is_self else msg.reply
+    spec = getfullargspec(func.__wrapped__).args
+    await func(**{k: v for k, v in kwargs.items() if k in spec})
+
+
+@Client.on_message(
+    filters.command("eval")
+    & SUDOERS
+)
+async def executor(client, message):
+    m = message
+    _ = client
+    if len(message.command) < 2:
+        return await edit_or_reply(
+            message, text="__Give me some command to execute.__"
+        )
+    try:
+        cmd = message.text.split(" ", maxsplit=1)[1]
+    except IndexError:
+        return await message.delete()
+    t1 = time()
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = StringIO()
+    redirected_error = sys.stderr = StringIO()
+    stdout, stderr, exc = None, None, None
+    try:
+        await aexec(cmd, client, message)
+    except Exception:
+        exc = traceback.format_exc()
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success"
+    final_output = f"**OUTPUT**:\n`{evaluation.strip()}`"
+    if len(final_output) > 4096:
+        filename = "output.txt"
+        with open(filename, "w+", encoding="utf8") as out_file:
+            out_file.write(str(evaluation.strip()))
+        t2 = time()
+        await message.reply_document(
+            document=filename,
+            caption=f"**INPUT:**\n`{cmd[0:980]}`\n\n**OUTPUT:**\n`Attached Document`",
+            quote=False,
+        )
+        await message.delete()
+        os.remove(filename)
+    else:
+        t2 = time()
+        await edit_or_reply(
+            message, text=final_output
+        )
