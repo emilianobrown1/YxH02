fa8ac81e8b320470043aca392d08d5569af06ed3
Alpha <124826956+imr-16@users.noreply.github.com>
2024-04-03 22:44:05 +0530

    Update user.py

--- YxH/Class/user.py
+++ YxH/Class/user.py
@@ -1,5 +1,6 @@
 from YxH.Database import db
 import pickle
+import time
 
 class User:
   def __init__(self, user):
@@ -15,6 +16,8 @@ class User:
     self.treasure_state = False # Locked
     self.treasure = [] # [coins, gems, crystals]
     self.store = {} # {date: [id1, id2, id3]}
+    self.gender = 0 # {0: None, 1: Male, -1: Female}
+    self.init_time = time.time() # now
 
   async def update(self):
     await db.users.update_one(
@@ -22,3 +25,6 @@ class User:
       {'$set': {'info': pickle.dumps(self)}},
       upsert=True
     )
+
+  def get_old(self) -> int:
+    return int((time.time() - self.init_time) / 86400)
