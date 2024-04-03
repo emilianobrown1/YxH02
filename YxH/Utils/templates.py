ac397c63b87d9ccb22680b0f0afff1718eabf6e8
Alpha <124826956+imr-16@users.noreply.github.com>
2024-04-03 22:39:23 +0530

    Update templates.py

--- YxH/Utils/templates.py
+++ YxH/Utils/templates.py
@@ -2,5 +2,7 @@ from ..Database.characters import get_anime_character
 
 async def get_anime_image_and_caption(id: int) -> str:
   c = await get_anime_character(id)
-  cap = "🎭 𝙉𝘼𝙈𝙀 : {}\n\n🎖𝘼𝙉𝙄𝙈𝙀 : {}\n\n💰 𝙋𝙍𝙄𝘾𝙀 : {} 💎\n\n🆔 : {}".format(c.name, c.anime, c.price, c.id)
-  return cap
+  return "🎭 𝙉𝘼𝙈𝙀 : {}\n\n🎖𝘼𝙉𝙄𝙈𝙀 : {}\n\n💰 𝙋𝙍𝙄𝘾𝙀 : {} 💎\n\n🆔 : {}".format(c.name, c.anime, c.price, c.id)
+
+async def xprofile_template(user):
+  return f"User: {user.user.first_name}\n\nGender: {user.gender}\n\nID: {user.user.id}\nOld: {user.get_old()}\n\nCrystals: {user.crystals} 🔮\nGems: {user.gems} 💎\nCoins: {user.coins} 🪙\nTreasure: {"Locked" if not user.treasure_state else "0, 0, 0"}\n\nCollected characters: {len(user.collection)}"
