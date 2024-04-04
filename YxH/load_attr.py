e179612b23fd68f822810354e74309075c5699a4
Alpha <124826956+imr-16@users.noreply.github.com>
2024-04-04 04:32:46 +0000

    uff

--- YxH/load_attr.py
+++ YxH/load_attr.py
@@ -1,12 +1,11 @@
 from .Database.users import get_user
 from .Class.user import User
 
-attr = [x for x in dir(User) if not callable(x)]
 ex_user = User("YxH")
+attr = [x for x in dir(ex_user) if not callable(x)]
 
 async def load_attr(user_id: int):
   user = await get_user(user_id)
-  user_attr = [x for x in dir(user) if not callable(x)]
   for x in attr:
     if hasattr(user, x):
       continue
