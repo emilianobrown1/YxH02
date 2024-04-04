af2d5fcdf752b8d4057488b084bbebe202238208
Alpha <124826956+imr-16@users.noreply.github.com>
2024-04-04 16:37:17 +0530

    Update xprofile.py

--- YxH/Plugins/xprofile.py
+++ YxH/Plugins/xprofile.py
@@ -1,8 +1,12 @@
 from pyrogram import Client, filters
 from . import YxH
 from ..Utils.templates import xprofile_template
+from ..Utils.markups import xprofile_markup
 
 @Client.on_message(filters.command("xprofile"))
 @YxH()
 async def xprofile(_, m, u):
-  await m.reply(xprofile_template(u))
+  await m.reply(
+    xprofile_template(u),
+    reply_markup=xprofile_markup(u)
+  )
