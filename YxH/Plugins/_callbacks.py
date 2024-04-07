0207450babd3b8ce47ad5e524b846844bc762500
Alpha <124826956+imr-16@users.noreply.github.com>
2024-04-07 18:45:41 +0530

    Update _callbacks.py

--- YxH/Plugins/_callbacks.py
+++ YxH/Plugins/_callbacks.py
@@ -1,9 +1,8 @@
 from pyrogram import Client
 from ..Database.users import get_user
-from ..Utils.markups import gender_markup, xprofile_markup
-from ..Utils.templates import xprofile_template
-from ..Utils.markups import store_markup
-from ..Utils.templates import get_anime_image_and_caption
+from ..Utils.markups import gender_markup, xprofile_markup, store_markup
+from ..Utils.templates import xprofile_template, get_anime_image_and_caption
+from pyrogram.types import InputMediaPhoto as imp
 from ..Utils.datetime import get_date
 
 @Client.on_callback_query()
@@ -53,4 +52,4 @@ async def cbq(_, q):
     image, caption = await get_anime_image_and_caption(chars[page-1])
     markup = store_markup(actual, page)
     await q.answer()
-    await q.edit_message_media(image, caption, reply_markup=markup)
+    await q.edit_message_media(imp(image, caption=caption), reply_markup=markup)
