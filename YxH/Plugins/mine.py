8c336a34c40c02371d531a1feeac3669f026bfed
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-05-03 00:24:15 +0530

    Update mine.py

--- YxH/Plugins/mine.py
+++ YxH/Plugins/mine.py
@@ -3,7 +3,7 @@ from . import YxH, get_date, get_week
 from ..Utils.templates import xprofile_template
 from ..Utils.markups import xprofile_markup
 import random
-from .bonus import today
+from .bonus import 
 
 @Client.on_message(filters.command("mine") & filters.group)
 @YxH()
