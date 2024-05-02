f9e5680f3bb1cdd024662d3ae3a26af2fa3cda92
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-05-02 23:06:52 +0530

    Update superxshop.py

--- YxH/Plugins/superxshop.py
+++ YxH/Plugins/superxshop.py
@@ -4,11 +4,7 @@ import random
 from ..Utils.markups import store_markup
 from ..Utils.templates import get_anime_image_and_caption
 
-RARITY_LEVELS = {
-    "Normal"
-    "Rare"
-    "Epic" 
-}
+
 @Client.on_message(filters.command("superxshop"))
 @YxH(private=False)
 async def sxs(_, m, u):
@@ -19,7 +15,6 @@ async def sxs(_, m, u):
     for i in range(1, cc+1):
       if len(store) == 3:
         break
-rarity = random.choices(list(RARITY_LEVELS.keys()), weights=list(RARITY_LEVELS.values()), k=1)[0]
 
 character_name, anime_name, price = await get_character_info(i)
 
