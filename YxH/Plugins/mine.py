21adaee41297b10e17d48010b62d73e0b5ad0ed7
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-05-03 00:34:27 +0530

    Update mine.py

--- YxH/Plugins/mine.py
+++ YxH/Plugins/mine.py
@@ -3,7 +3,7 @@ from . import YxH, get_date, get_week
 from ..Utils.templates import xprofile_template
 from ..Utils.markups import 
 import  xprofile_markup
-import bonus 
+from bonus 
 import random
 
 
