1227629c31da59b98552fe901122fd4d7c09291e
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-05-03 07:25:01 +0530

    Update mine.py

--- YxH/Plugins/mine.py
+++ YxH/Plugins/mine.py
@@ -1,6 +1,5 @@
 from pyrogram import Client, filters
 from . import YxH, get_date, get_week
-from ..Utils.templates import 
 import random
 
 @Client.on_message(filters.command("mine") & filters.group)
