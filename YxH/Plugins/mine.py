2db368b8af9555028d3c71ba94b9dcfda5f7ec23
Alpha <124826956+imr-16@users.noreply.github.com>
2024-05-04 17:33:03 +0530

    Update mine.py

--- YxH/Plugins/mine.py
+++ YxH/Plugins/mine.py
@@ -1,26 +1,27 @@
 from pyrogram import Client, filters
-from . import YxH, get_date, get_week
+from . import YxH
 import random
 
-@Client.on_message(filters.command("mine") & filters.group)
-@YxH()
+lis: list[int] = list(range(10, 51))
+
+@Client.on_message(filters.command("mine"))
+@YxH(private=False)
 async def mine(_, m, user):
-    
     min_gold_required = 500
-    
-    
-    if user.gold < min_gold_required:
-        return await m.reply("You need at least 500 gold to start mining.")
-    
-    
-    success = random.randint(0, 100) < 20  
-    if success:
-        
-        user.gold_mines += 1  
-        await m.reply("You've struck gold! Reward: Gold Mine")
+    try:
+        inp: int = int(m.text.split()[1])
+    except:
+        return await m.reply('`/mine 1000`')
+    if inp > user.gold:
+        return await m.reply(f'You only having `{user.gold}` gold.')
+    if inp < min_gold_required:
+        return await m.reply("You need at least `500` gold to start mining.")
+    success = random.choice([True, False])
+    gold: int = int((inp * random.choice(lis))/100)
+    if success:  
+        user.gold += gold  
+        await m.reply(f"You've struck gold! Reward: `{gold}` gold.")
     else:
-        
-        await m.reply("No luck this time, keep mining!")
-    
-    # Update the user's data
+        user.gold -= gold 
+        await m.reply(f"No luck this time, keep mining!, Lost: `{gold}` gold.")
     return await user.update()
