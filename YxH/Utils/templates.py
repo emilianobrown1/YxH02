dc7e20901616b51496850642b4e92d677fbc516a
Alpha <124826956+imr-16@users.noreply.github.com>
2024-05-11 11:57:13 +0530

    Update templates.py

--- YxH/Utils/templates.py
+++ YxH/Utils/templates.py
@@ -7,12 +7,15 @@ async def get_anime_image_and_caption(id: int) -> str:
 def xprofile_template(user):
   return f'User: {user.user.first_name}\n\nGender: {user.gl[user.gender]}\n\nID: {user.user.id}\nOld: {user.get_old()}\n\nCrystals: {user.crystals} 🔮\nGems: {user.gems} 💎\nGold: {user.gold} 📯\nTreasure: {"Locked" if not user.treasure_state else "0, 0, 0"}\n\nCollected characters: {len(user.collection)}'
 
-def acollection_template(lis: list[dict]) -> str:
+def acollection_template(lis: list[dict], no: list[int]) -> str:
     txt = ''
-    for x in lis:
-        txt += '🎭 𝙉𝘼𝙈𝙀 : ' + str(x.get('name', '')) + '\n\n'
-        txt += '🎖𝘼𝙉𝙄𝙈𝙀 : ' + str(x.get('anime', '')) + '\n\n'
-        txt += '💰 𝙋𝙍𝙄𝘾𝙀 : ' + str(x.get('price', '')) + '\n\n'
-        txt += '♦️ Epic\n\n'
-        txt += '🆔 : ' + str(x.get('id', '')) + '\n\n'
+    l = len(lis)
+    for y in range(l):
+        x = lis[y]
+        o = no[y]
+        txt += '🎭 𝙉𝘼𝙈𝙀 : ' + str(x.get('name', '')) + '\n'
+        txt += '🎖  𝘼𝙉𝙄𝙈𝙀 : ' + str(x.get('anime', '')) + '\n'
+        txt += '💰 𝙋𝙍𝙄𝘾𝙀 : ' + str(x.get('price', '')) + '\n'
+        txt += '♦️ Epic\n'
+        txt += '🆔 : ' + str(x.get('id', '')) + f' (x{o})' + '\n\n'
     return txt
