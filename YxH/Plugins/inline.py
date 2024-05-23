4d41b948357a59c05eebe0233ded3a876cbbef5f
Alpha <124826956+imr-16@users.noreply.github.com>
2024-05-23 19:39:07 +0530

    Update inline.py

--- YxH/Plugins/inline.py
+++ YxH/Plugins/inline.py
@@ -1,22 +1,34 @@
 from ..Database.characters import get_all
+from ..Database.users import get_user
 from pyrogram import Client, filters
 from pyrogram.types import InlineQuery
 import asyncio
 
-answers = []
+answers = {}
 
 async def load():
     global answers
     if not answers:
         all = await get_all()
-        new = [x.inline for x in all.values() if hasattr(x, 'inline')]
+        new = {x.id: x.inline for x in all.values() if hasattr(x, 'inline')}
         answers = new
 
 @Client.on_inline_query()
 async def inl(_, i: InlineQuery):
+    if i.query.startswith("collection_"):
+        try:
+            user_id = int(i.query.split("_")[1])
+        except:
+            user_id = i.from_user.id
+        user = await get_user(user_id)
+        final_answers = []
+        for x in user.collection:
+            final_answers.append(answers[x])
+    else:
+        final_answers = answers
     offset = int(i.offset or 0)
     NEXT = 30
-    images = answers[offset: offset+NEXT]
+    images = final_answers[offset: offset+NEXT]
     await i.answer(results=images, is_gallery=True, cache_time=1, next_offset=str(offset + NEXT))
 
 asyncio.create_task(load())
