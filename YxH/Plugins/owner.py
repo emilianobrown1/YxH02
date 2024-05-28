0c3abd58afa6560f0bc828ec8a812634cc37a9b7
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-05-28 23:09:42 +0530

    Update owner.py

--- YxH/Plugins/owner.py
+++ YxH/Plugins/owner.py
@@ -1,10 +1,38 @@
 from config import SUDO_USERS
 from pyrogram import Client, filters
+from ..Database.users import get_user
 
-async def get_user(_, m):
-    if m.reply_to_message:
-        return m.reply_to_message.from_user.id
+@Client.on_message(filters.command("gold") & filters.user(SUDO_USERS))
+async def gold(_, m):
+    spl = m.text.split()
     try:
-        return int(m.text.split()[1])
+        id, amount = m.reply_to_message.from_user.id, int(spl[1]) if m.reply_to_message else int(spl[1]), int(spl[2])
     except:
-        await m.reply("Either reply to an user or provide ID.")
+        return await m.reply("Usage:\n\n/gold <id> <amount>\n/gold <amount> (Reply to an user)")
+    u = await get_user(id)
+    u.gold += amount
+    await m.reply(f"Added `{amount}`")
+    await u.update()
+    
+@Client.on_message(filters.command("gems") & filters.user(SUDO_USERS))
+async def gems(_, m):
+    spl = m.text.split()
+    try:
+        id, amount = m.reply_to_message.from_user.id, int(spl[1]) if m.reply_to_message else int(spl[1]), int(spl[2])
+    except:
+        return await m.reply("Usage:\n\n/gems <id> <amount>\n/gems <amount> (Reply to an user)")
+    u = await get_user(id)
+    u.gems += amount
+    await m.reply(f"Added `{amount}`")
+    await u.update()
+    
+@Client.on_message(filters.command("crystals") & filters.user(SUDO_USERS))
+async def crystals(_, m):
+    spl = m.text.split()
+    try:
+        id, amount = m.reply_to_message.from_user.id, int(spl[1]) if m.reply_to_message else int(spl[1]), int(spl[2])
+    except:
+        return await m.reply("Usage:\n\n/crystals <id> <amount>\n/crystals <amount> (Reply to an user)")
+    u = await get_user(id)
+    u.crystals += amount
+    await m.reply(f"Added `{amount}`")
