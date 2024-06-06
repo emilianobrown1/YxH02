7e3f5a98d1122df1f031baa2a2c6a16089c93f17
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-06 18:19:12 +0530

    Update load_attr.py

--- YxH/load_attr.py
+++ YxH/load_attr.py
@@ -1,12 +1,15 @@
 from .Database.users import get_user
 from .Database.chats import get_chat
-from .Class import User, Chat
+from .Database.clan import get_clan
+from .Class import User, Chat, Clan
 
 ex_user = User('YxH')
 ex_chat = Chat('YxH')
+ex_clan = Clan(-69, "YxH", 0)
 
 attr = [x for x in dir(ex_user) if not callable(x)]
 chat_attr = [x for x in dir(ex_chat) if not callable(x)]
+clan_attr = [x for x in dir(ex_clan) if not callable(x)]
 
 async def load_attr(user_id: int) -> User:
   user = await get_user(user_id)
@@ -25,3 +28,12 @@ async def load_chat_attr(chat_id: int) -> Chat:
     setattr(chat, x, getattr(ex_chat, x))
   await chat.update()
   return chat
+  
+async def load_clan_attr(clan_id: int) -> Clan:
+  clan = await get_clan(clan_id)
+  for x in clan_attr:
+    if hasattr(clan, x):
+      continue
+    setattr(clan, x, getattr(ex_clan, x))
+  await clan.update()
+  return clan
