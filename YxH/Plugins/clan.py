f98d9e499e93172f62ebac56f48028630216a8ee
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-06 13:47:57 +0530

    Update clan.py

--- YxH/Plugins/clan.py
+++ YxH/Plugins/clan.py
@@ -1,9 +1,11 @@
 from pyrogram import Client, filters
 from ..Class.clan import Clan
+from ..Database.clan import get_clan
 from ..universal_decorator import YxH
 
 @Client.on_message(filters.command("clan"))
 @YxH()
 async def c(_, m, u):
-    if not u.clan:
+    if not u.clan_id:
         return await m.reply("You are not placed in any clan, You can join a clan or create one using /create.")
+    
