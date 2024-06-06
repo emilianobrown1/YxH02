48fa701cea24b17fe550046747499ac82fa84fe5
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-06 13:35:21 +0530

    Update clan.py

--- YxH/Class/clan.py
+++ YxH/Class/clan.py
@@ -1,5 +1,11 @@
+from ..Database.clan import db
+import pickle
+
 class Clan:
     def __init__(self, clan_id, owner):
         self.clan_id = clan_id
         self.owner = owner
         self.members = []
+        
+    async def update(self):
+        await db.update_one({"clan_id": self.clan_id}, {"$set": {"info": pickle.dumps(self)}}, upsert=True)
