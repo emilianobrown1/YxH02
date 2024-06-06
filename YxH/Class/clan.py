e573d2c678d2a080d2d92e5675c569af0bf67d39
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-06 13:49:25 +0530

    Update clan.py

--- YxH/Class/clan.py
+++ YxH/Class/clan.py
@@ -2,8 +2,9 @@ from ..Database.clan import db
 import pickle
 
 class Clan:
-    def __init__(self, clan_id, owner):
+    def __init__(self, clan_id, clan_name, owner):
         self.clan_id = clan_id
+        self.clan_name = clan_name
         self.owner = owner
         self.members = []
         
