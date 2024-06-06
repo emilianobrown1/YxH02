ede47bf7fe46bf939acbedae153a4ab0554729a4
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-06 13:53:11 +0530

    Update clan.py

--- YxH/Plugins/clan.py
+++ YxH/Plugins/clan.py
@@ -9,3 +9,7 @@ async def c(_, m, u):
     if not u.clan_id:
         return await m.reply("You are not placed in any clan, You can join a clan or create one using /create.")
     
+@Client.on_message(filters.command("create"))
+@YxH()
+async def cr(_, m, u):
+   ...
