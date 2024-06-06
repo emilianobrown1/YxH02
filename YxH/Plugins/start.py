ca371f53a57fcb0b90a64b3b1447de51459a20cf
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-06 17:11:39 +0530

    Update start.py

--- YxH/Plugins/start.py
+++ YxH/Plugins/start.py
@@ -3,11 +3,33 @@ from ..Class import User
 from ..Utils.strings import start_text
 from ..Utils.markups import start_markup
 from ..Database.users import get_user
+from ..Database.clan import get_clan
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start(_, m):
+  user = await get_user(m.from_user.id)
   await m.reply_photo("Images/start.JPG", start_text.format(m.from_user.first_name), reply_markup=await start_markup())
-  user = m.from_user
-  if not await get_user(user.id):
-    u = User(user)
+  if not user:
+    u = User(m.from_user)
     await u.update()
+    
+async def join_clan(_, m, user):
+    if "join_" in m.text:
+    id = int(m.text.split("_")[1])
+    if user.clan_id:
+        if user.clan_id = id:
+            return await m.reply("You are already in the clan you want to join.")
+        else:
+            return await m.reply("You are already in a clan.")
+    if user.crystals < 100:
+        return await m.reply(f"You need `{100-user.crystals}` more crystal(s) to join.")
+    clan = await get_clan(id)
+    if clan.anyone_can_join:
+        if len(clan.members) >= 15:
+            return await m.reply("Clan is full!")
+        user.clan_id = id
+        user.crystals -= 100
+        clan.members.append(m.from_user.id)
+        await m.reply(f"You have joined **{clan.name}**.")
+        await user.update()
+        await clan.update()
