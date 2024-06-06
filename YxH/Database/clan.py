1568b70db91500c0fc6389e3a166ccf485fa5967
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-06 13:50:23 +0530

    Update clan.py

--- YxH/Database/clan.py
+++ YxH/Database/clan.py
@@ -7,3 +7,8 @@ async def get_clan(clan_id):
     x = await db.find_one({"clan_id": clan_id})
     if x:
         pickle.loads(return x["info"])
+        
+async def get_clans_count() -> int:
+    x = db.find()
+    x = await x.to_list(length=None)
+    return len(x)
