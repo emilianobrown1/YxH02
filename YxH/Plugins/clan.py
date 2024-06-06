bdc9d21d18c02dd508fc4e34aaddb295bcb0bfe9
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-06 16:56:50 +0530

    Update clan.py

--- YxH/Plugins/clan.py
+++ YxH/Plugins/clan.py
@@ -16,9 +16,9 @@ Members: `{}/15`
 Join our mighty clan and conquer the fantasy world together! 💪🌟
 """
 
-@Client.on_message(filters.command("clan"))
+@Client.on_message(filters.command("myclan"))
 @YxH()
-async def c(_, m, u):
+async def myc(_, m, u):
   if not u.clan_id:
     return await m.reply("You are not placed in any clan, You can join a clan or create one using /create.")
   clan = await get_clan(u.clan_id)
