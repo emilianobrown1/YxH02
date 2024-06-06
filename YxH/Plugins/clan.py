4f388b4e68b4330420e107f1e8e96904c8712467
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-06 15:59:50 +0530

    Update clan.py

--- YxH/Plugins/clan.py
+++ YxH/Plugins/clan.py
@@ -7,8 +7,9 @@ from pyrogram.types import InlineKeyboardMarkup as ikm, InlineKeyboardButton as 
 @Client.on_message(filters.command("clan"))
 @YxH()
 async def c(_, m, u):
-    if not u.clan_id:
-        return await m.reply("You are not placed in any clan, You can join a clan or create one using /create.")
+  if not u.clan_id:
+    return await m.reply("You are not placed in any clan, You can join a clan or create one using /create.")
+  
     
 @Client.on_message(filters.command("create"))
 @YxH()
