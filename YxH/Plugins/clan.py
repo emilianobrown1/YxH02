23483c753bd2737d04f06c6900f7b73e1f0859ce
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-06 14:04:30 +0530

    Update clan.py

--- YxH/Plugins/clan.py
+++ YxH/Plugins/clan.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from ..Class.clan import Clan
-from ..Database.clan import get_clan
+from ..Database.clan import get_clan, get_clans_count
 from ..universal_decorator import YxH
 
 @Client.on_message(filters.command("clan"))
@@ -12,6 +12,15 @@ async def c(_, m, u):
 @Client.on_message(filters.command("create"))
 @YxH()
 async def cr(_, m, u):
-   if u.clan_id:
-      return
-   
+  if u.clan_id:
+    return
+  try:
+    clan_name = "".join(m.text.split()[1:])
+  except:
+    return await m.reply("Usage: /create <clan name>")
+  if u.crystals < 500:
+    return await m.reply(f"You need `{500-u.crystals}` more crystal(s) to create a clan.")
+  u.crystals -= 500
+  clan_id = await get_clans_count() + 1
+  u.clan_id = clan_id
+  return await m.reply(f"(**{clan_name}**) Clan has been created.")
