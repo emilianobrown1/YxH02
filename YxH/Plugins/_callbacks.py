6687e1f42b0e99062561422ddf1619be6b0ceead
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-07 16:01:25 +0530

    Update _callbacks.py

--- YxH/Plugins/_callbacks.py
+++ YxH/Plugins/_callbacks.py
@@ -27,7 +27,7 @@ from .spinxwin import spin_cbq
 
 from .bonus import claim_cbq
 from .equipments import e_cbq
-from .clan import settings_cbq, clanback_cbq
+from .clan import settings_cbq, clanback_cbq, members_cbq
 
 @Client.on_callback_query()
 async def cbq(_, q: CallbackQuery):
@@ -161,5 +161,7 @@ async def cbq(_, q: CallbackQuery):
     await settings_cbq(_, q, u)
   elif data.startswith("clanback"):
     await clanback_cbq(_, q, u)
+  elif data.startswith("members"):
+    await members_cbq(_, q, u):
   else:
     return await q.answer("Under maintenance.", show_alert=True)
