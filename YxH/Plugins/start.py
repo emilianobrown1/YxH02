d27f83c13fedb12d50fb9e7405afb3643f8e44e2
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-07 23:59:03 +0530

    Update start.py

--- YxH/Plugins/start.py
+++ YxH/Plugins/start.py
@@ -3,13 +3,15 @@ from ..Class import User
 from ..Utils.strings import start_text
 from ..Utils.markups import start_markup
 from ..Database.users import get_user
-from .clan import join_clan
+from ..Database.clan import get_clan
+from .clan import clan_info
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start(_, m):
   user = await get_user(m.from_user.id)
   if "join_" in m.text:
-      return await join_clan(_, m, user)
+      txt, markup = clan_info(await get_clan(int(m.text.split("_")[1]), m.from_user.id))
+      return await m.reply(txt, reply_markup=markup)
   await m.reply_photo("Images/start.JPG", start_text.format(m.from_user.first_name), reply_markup=await start_markup())
   if not user:
     u = User(m.from_user)
