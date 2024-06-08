785bbee9cd452d2ad1b25883189fddad8312c9bd
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-08 18:33:19 +0530

    Update start.py

--- YxH/Plugins/start.py
+++ YxH/Plugins/start.py
@@ -8,11 +8,11 @@ from .clan import clan_info
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start(_, m):
-  user = await get_user(m.from_user.id)
   if "clan_" in m.text:
       txt, markup = await clan_info(await get_clan(int(m.text.split("_")[1])), m.from_user.id)
       return await m.reply(txt, reply_markup=markup)
   await m.reply_photo("Images/start.JPG", start_text.format(m.from_user.first_name), reply_markup=await start_markup())
+  user = await get_user(m.from_user.id)
   if not user:
     u = User(m.from_user)
     await u.update()
