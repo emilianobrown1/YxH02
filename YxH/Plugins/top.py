17e7aa6060dee11db84bdebd24b402d44e32e0ac
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-13 13:34:22 +0530

    Update top.py

--- YxH/Plugins/top.py
+++ YxH/Plugins/top.py
@@ -1,3 +1,18 @@
 from pyrogram import Client, filters
 from ..Database.users import get_all_users
+from ..universal_decorator import YxH
+import heapq
 
+def key_func(user):
+    return sum([user.mine[x] for x in user.mine])
+    
+@Client.on_message(filters.command("top"))
+@YxH()
+async def top(_, m, u):
+    users = await get_all_users()
+    top10 = heapq.nlargest(10, users, key=key_func)
+    txt = "**Top Miners**"
+    txt += "\n\n"
+    for x, y in emumerate(top10):
+        txt += f"`{x+1}. **{y.user.first_name}**\n"
+    await m.reply(txt)
