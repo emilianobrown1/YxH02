6929861a900f5d7a985333df1ff423b3eac76db3
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-13 19:04:45 +0530

    Update top.py

--- YxH/Plugins/top.py
+++ YxH/Plugins/top.py
@@ -2,13 +2,19 @@ from pyrogram import Client, filters
 from ..Database.users import get_all_users
 from ..universal_decorator import YxH
 import heapq
+from ..load_attr import load_attr
     
+def key_func(user):
+    try:
+        return sum([user.mine[x] for x in user.mine])
+    except AttributeError:
+        user = await load_attr(user.user.id)
+        return sum([user.mine[x] for x in user.mine])
+
 @Client.on_message(filters.command("top"))
 @YxH()
 async def top(_, m, u):
     users = await get_all_users()
-    def key_func(user):
-        return sum([user.mine[x] for x in user.mine])
     top10 = heapq.nlargest(10, users, key=key_func)
     txt = "**Top Miners**"
     txt += "\n\n"
