1c9b52d8b07bc73260af9cab077aa1ffd75f0a5b
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-13 13:35:45 +0530

    Update users.py

--- YxH/Database/users.py
+++ YxH/Database/users.py
@@ -8,3 +8,8 @@ async def get_user(user_id):
   if x:
     return pickle.loads(x['info'])
   return None
+  
+async def get_all_users():
+  x = db.find()
+  x = await x.to_list(length=None)
+  return [pickle.loads(y["info"]) for y in x]
