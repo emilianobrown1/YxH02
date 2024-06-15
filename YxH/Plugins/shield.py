2483e6b7a2cbb9e51b6e3d5b5573eba1d06a9a70
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-16 00:22:47 +0530

    Update shield.py

--- YxH/Plugins/shield.py
+++ YxH/Plugins/shield.py
@@ -1,7 +1,13 @@
 from pyrogram import Client, filters
+from . import YxH
 
 shields = {
     "Gold": [500000000, 24*3600],
     "Silver": [300000000, 12*3600],
     "Iron": [100000000, 8*3600]
 }
+
+@Client.on_message(filters.command("shield"))
+@YxH()
+async def sh(_, m, u):
+    ...
