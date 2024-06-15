6a005aad491ea3bd1bd4f357afd0c05786ee6076
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-15 22:38:14 +0530

    Update __init__.py

--- YxH/Plugins/__init__.py
+++ YxH/Plugins/__init__.py
@@ -4,14 +4,3 @@ from ..Database.users import get_user
 from ..Database.chats import get_chat
 from ..Database.characters import get_anime_character, anime_characters_count
 from ..universal_decorator import YxH
-import requests
-from PIL import Image
-from io import BytesIO
-
-def download_image(url, id) -> str:
-    x = requests.get(url).content
-    im = Image.open(BytesIO(x))
-    path = f"Characters/{im}.jpg"
-    im.save(path)
-    return path
-    
