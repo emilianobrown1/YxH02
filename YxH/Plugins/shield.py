154f1f905bde76c99351662f1044946b26e70d24
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-17 10:14:55 +0530

    Update shield.py

--- YxH/Plugins/shield.py
+++ YxH/Plugins/shield.py
@@ -1,12 +1,23 @@
 from pyrogram import Client, filters
 from . import YxH, grt
+from pyrogram.types import (
+    InlineKeyboardMarkup as ikm,
+    InlineKeyboardButton as ikb
+)
 import time
 
 shields = {
-    "Gold": [500000000, 24*3600],
-    "Silver": [300000000, 12*3600],
-    "Iron": [100000000, 8*3600]
+    "Gold": [500000000, 24],
+    "Silver": [300000000, 12],
+    "Iron": [100000000, 8]
 }
+
+def build_markup(user_id):
+    lis = []
+    for x in shields:
+        lis.append(ikb(f"{x} ({shields[x][1]}h) - {shields[x][1]} Gold"))
+    lis.append(ikb("Close", callback_data=f"close_{user_id}"))
+    return ikm([lis])
 
 @Client.on_message(filters.command("shield"))
 @YxH()
@@ -17,3 +28,4 @@ async def sh(_, m, u):
     if t.shield:
         left = t.shield[0] - int(time.time()-t.shield[1])
         return await m.reply(f"You already having a shield equipped and will be expired after `{grt(left)}`.")
+    
