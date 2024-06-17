c91abdbd5bbc40573427441af8a972daf956edd6
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-17 10:06:31 +0530

    Update __init__.py

--- YxH/Plugins/__init__.py
+++ YxH/Plugins/__init__.py
@@ -4,3 +4,26 @@ from ..Database.users import get_user
 from ..Database.chats import get_chat
 from ..Database.characters import get_anime_character, anime_characters_count
 from ..universal_decorator import YxH
+
+def grt(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for i in range(len(time_list)):
+        time_list[i] = str(time_list[i]) + time_suffix_list[i]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+    return ping_time
