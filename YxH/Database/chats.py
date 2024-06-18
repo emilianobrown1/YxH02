2abad2c857cb1272c644e42cfd669fd43425ec40
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-18 23:03:40 +0530

    Update chats.py

--- YxH/Database/chats.py
+++ YxH/Database/chats.py
@@ -8,3 +8,8 @@ async def get_chat(chat_id):
   if x:
     return pickle.loads(x['info'])
   return None
+  
+async def get_all_chats():
+  x = db.find()
+  x = await x.to_list(length=None)
+  return [pickle.loads(y["info"]) for y in x]
