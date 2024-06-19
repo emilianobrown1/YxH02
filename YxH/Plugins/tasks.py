6b94418c5637dc197199eaf1f66ae5e22c6e331e
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-19 14:55:43 +0530

    Update tasks.py

--- YxH/Plugins/tasks.py
+++ YxH/Plugins/tasks.py
@@ -1,5 +1,5 @@
 from ..Database.users import get_all_users
-from ..Database.clans import get_clans
+from ..Database.clan import get_clans
 from ..Database.chats import get_all_chats
 from ..load_attr import load_attr, load_clan_attr, load_chat_attr
 import asyncio
