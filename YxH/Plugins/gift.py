2d4c97725302d3cb0484451cdd5fd0a6398f20cb
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-19 16:48:35 +0530

    Update gift.py

--- YxH/Plugins/gift.py
+++ YxH/Plugins/gift.py
@@ -1,9 +1,13 @@
 from pyrogram import Client, filters
-from . import YxH, get_date, get_user
+from . import YxH, get_date, get_user, get_date
 
 @Client.on_message(filters.command("xgift"))
-@YxH()
+@YxH(min_old=3)
 async def xgift(_, m, u):
+    date = get_date()
+    gifts = u.gifts_sent.get(date, 0)
+    if gifts >= 5:
+        return await m.reply("You have reached the maximum limit of gifting for today.")
     if not m.reply_to_message or not m.reply_to_message.from_user:
         return await m.reply("Reply to an user.")
     t = await get_user(m.reply_to_message.from_user.id)
@@ -13,6 +17,15 @@ async def xgift(_, m, u):
         id = int(m.text.split()[1])
     except:
         return await m.reply("Usage: /xgift [character id]")
-    have = u.collection.get(id, 0)
-    if have == 0:
+    if not id in u.collection:
         return await m.reply("You do not own this character.")
+    if u.collection[id] == 1:
+        u.collection.pop(id)
+    else:
+        u.collection[id] -= 1
+    t.collection[id] = t.collection.get(id, 0) + 1
+    u.gifts_sent[date] = gifts + 1
+    await t.update()
+    await u.update()
+    await m.reply(f"Successfully gifted the character of ID `{id}` to {t.user.mention}.\n\nGifts left for today: `{4-gifts}`.")
+    await _.send_message(f"{u.user.mention} has gifted you the character of ID `{id}`.")
