3efd52a9d7b7cb159bb7193e4c1a5a5b4f6e734b
Alpha <124826956+Alpha-C2@users.noreply.github.com>
2024-06-20 13:55:59 +0530

    Update prof.py

--- YxH/Plugins/prof.py
+++ YxH/Plugins/prof.py
@@ -1,11 +1,17 @@
 from pyrogram import Client, filters
-from . import YxH
+from . import YxH, get_anime_character
 from ..Utils.templates import xprofile_template
 from ..Utils.markups import xprofile_markup
 
 @Client.on_message(filters.command("xprofile"))
 @YxH()
 async def xprof(_, m, u):
+  if u.favourite_character:
+    return await m.reply_photo(
+      (await get_anime_character(u.favourite_character)).image,
+      caption=await xprofile_template(u),
+      reply_markup=xprofile_markup(u)
+    )
   return await m.reply(
     await xprofile_template(u),
     reply_markup=xprofile_markup(u)
