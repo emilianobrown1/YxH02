b171972272123f02a40efab2263738c687e55c5f
Alpha <124826956+Alpha-Like@users.noreply.github.com>
2024-06-26 19:51:43 +0530

    Create disabled.py

---
+++ YxH/Database/disabled.py
@@ -0,0 +1,13 @@
+from . import db
+
+db = db.disabled
+
+async def disable(comm: str) -> None:
+  await db.insert_one({'command': comm})
+
+async def get_disabled() -> list[str]:
+  x = db.find()
+  x = await db.to_list(length=None)
+  if not x:
+    return []
+  return [i['command'] for i in x]
