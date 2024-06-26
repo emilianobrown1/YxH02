486b6926a446e807dfeb8062749ac330db597769
Alpha <124826956+Alpha-Like@users.noreply.github.com>
2024-06-26 20:00:59 +0530

    Update disabled.py

--- YxH/Database/disabled.py
+++ YxH/Database/disabled.py
@@ -5,6 +5,9 @@ db = db.disabled
 async def disable(comm: str) -> None:
   await db.insert_one({'command': comm})
 
+async def enable(comm: str) -> None:
+  await db.insert_one({'command': comm})
+
 async def get_disabled() -> list[str]:
   x = db.find()
   x = await db.to_list(length=None)
