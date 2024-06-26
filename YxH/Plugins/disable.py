8d431fee8d73040d750d4eb7ea42f79dd7b44c17
Alpha <124826956+Alpha-Like@users.noreply.github.com>
2024-06-26 20:00:32 +0530

    Update disable.py

--- YxH/Plugins/disable.py
+++ YxH/Plugins/disable.py
@@ -1,6 +1,23 @@
 from pyrogram import Client, filters
+from ..Database.disabled import disable, enable
+from . import YxH
 
 @Client.on_message(filters.command('disable'))
 @YxH(sudo=True)
 async def dis(_, m, u):
-  ...
+  try:
+    command = m.text.split()[1].lower()
+  except:
+    return
+  await disable(command)
+  await m.reply(f'Disabled `/{command}`.')
+
+@Client.on_message(filters.command('enable'))
+@YxH(sudo=True)
+async def ena(_, m, u):
+  try:
+    command = m.text.split()[1].lower()
+  except:
+    return
+  await enable(command)
+  await m.reply(f'Enabled `/{command}`.)
