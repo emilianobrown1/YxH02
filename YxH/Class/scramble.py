02ed5ab35e3c85cbc9af8e3f64a5a5e9eb9e8d5a
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-06-30 21:42:17 +0530

    Update Class

---
+++ YxH/Class/scramble.py
@@ -0,0 +1,12 @@
+def reset_daily_state(self):
+        if datetime.today().date() != self.last_played_date:
+            self.scramble_progress = {
+                'incorrect_attempts': 0,
+                'stops': 0,
+                'skips': 0,
+                'count': 0,
+                'completed': False,
+                'blocked_until': None
+            }
+            self.daily_skips = 0
+            self.last_played_date = datetime.today().date()
