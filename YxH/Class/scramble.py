3cf189dfa7db5fa788a56de2bf9be7b8d25a0018
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-06-30 21:48:00 +0530

    Update scramble.py

--- YxH/Class/scramble.py
+++ YxH/Class/scramble.py
@@ -1,8 +1,22 @@
 from ..Database.clan import db
 import pickle
 
-class Scramble: 
-def __init__reset_daily_state(self):
+from datetime import datetime
+
+class Scramble:
+    def __init__(self):
+        self.scramble_progress = {
+            'incorrect_attempts': 0,
+            'stops': 0,
+            'skips': 0,
+            'count': 0,
+            'completed': False,
+            'blocked_until': None
+        }
+        self.daily_skips = 0
+        self.last_played_date = datetime.today().date()
+
+    def reset_daily_state(self):
         if datetime.today().date() != self.last_played_date:
             self.scramble_progress = {
                 'incorrect_attempts': 0,
