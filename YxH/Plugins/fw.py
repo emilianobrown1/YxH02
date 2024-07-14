ec72560bfe4b4433fe1c10e3cf38a357d9c6841c
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-07-14 17:14:59 +0530

    Update fw.py

--- YxH/Plugins/fw.py
+++ YxH/Plugins/fw.py
@@ -11,7 +11,7 @@ count: dict[int, int] = {}
 
 me = None
 
-markup = IKM([[IKB("Join 🚀", url="https://t.me/+6GxvSweDrPUwZTc1")]])
+markup = IKM([[IKB("Join 🚀", url="https://t.me/YxH_Homeland")]])
 
 async def cwf(_, m):
     global count, me
