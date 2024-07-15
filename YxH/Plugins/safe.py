7880c5e861a56018a5005e437167b92da7ac8380
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-07-15 19:20:51 +0530

    Update safe.py

--- YxH/Plugins/safe.py
+++ YxH/Plugins/safe.py
@@ -3,6 +3,7 @@ from YxH.Database import db
 import pickle
 
 @Client.on_message(filters.command("safexgd"))
+@YxH()
 async def safegd(client, message):
     user = await get_user_from_message(message)
     if user is None:
