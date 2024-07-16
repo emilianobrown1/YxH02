3e7362c491c65f8da76e2c725af3fb4e8a8994f3
Alpha <124826956+Alpha-Like@users.noreply.github.com>
2024-07-16 19:37:18 +0530

    Update user.py

--- YxH/Class/user.py
+++ YxH/Class/user.py
@@ -15,7 +15,7 @@ class User:
     self.dbonus = {} # {date: False}
     self.blocked = False
     self.treasure_state = False # Locked
-    self.treasure = [] # [golds, gems, crystals]
+    self.treasure = [0, 0, 0] # [golds, gems, crystals]
     self.store = {} # {date: [id1, id2, id3]}
     self.store_purchases = {} # {date: [False, False, False]}
     self.gender = 0 # {0: None, 1: Male, -1: Female}
