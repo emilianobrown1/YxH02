e1fcc72b778fa5ea01485209a6d2bd47d3ff5a03
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-07-17 22:30:02 +0530

    Create wordle_image.py

---
+++ YxH/Plugins/wordle_image.py
@@ -0,0 +1,35 @@
+from PIL import Image, ImageDraw, ImageFont
+
+def colourify(im, word: str, text: str, height):
+    width = 18
+    wi = im.size[0]
+    d = ImageDraw.Draw(im)
+    f = ImageFont.truetype("./Fonts/font.ttf", 40)
+    for i in range(0, 5):
+        if text[i] == word[i]:
+            clr = "green"
+        else:
+            if text[i] in word:
+                clr = "yellow"
+            else:
+                clr = "white"
+        w, h = d.textsize(text[i].upper(), f)
+        d.text((width, height), text[i].upper(), fill=clr, font=f)
+        d.line((0, height + 5, wi, height + 5))
+        d.line((0, height + h + 5, wi, height + h + 5))
+        width += 70
+
+async def make_secured_image(user_id: int, word: str, lis: list) -> str:
+    f = ImageFont.truetype("./Fonts/font.ttf", 10)
+    le = len(lis)
+    if le == 1:
+        x = Image.open("Images/wordle.jpg")
+        un = "@YxHGameBot"  # Replace with your bot's username
+        ImageDraw.Draw(x).text((5, 5), un, fill="white", font=f)
+    else:
+        x = Image.open(f"user_images/{user_id}.jpg")
+    he = x.size[1]
+    he_co_or = (le * he / 7) - he / 14
+    colourify(x, word, lis[-1], he_co_or)
+    x.save(f"user_images/{user_id}.jpg")
+    return f"user_images/{user_id}.jpg"
