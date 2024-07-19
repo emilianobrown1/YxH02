d73da020f7ba986faf9cd390df9d1c8a5fd76d97
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-07-19 13:46:09 +0530

    Update wordle.py

--- YxH/Plugins/wordle.py
+++ YxH/Plugins/wordle.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from . import get_date, YxH
 from ..Class.wordle import wordle
-from ..Database.wordle import add_game, get_wordle_dic, get_avg, incr_game, get_today_games, get_all_games, add_crystal
+from ..Database.wordle import add_game, get_wordle_dic, get_avg, incr_game, get_today_games, get_all_games
 from ..Database.users import get_user, add_crystal
 from .wordle_image import make_secured_image
 from easy_words import words
