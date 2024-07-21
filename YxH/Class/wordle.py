6ff876ab6c940c7bba91e23958623cd624635fc3
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-07-21 13:17:32 +0530

    Update wordle.py

--- YxH/Class/wordle.py
+++ YxH/Class/wordle.py
@@ -1,19 +1,13 @@
 from ..Database.wordle import db
+from .user import User
 import time
-from datetime import datetime
 import pickle
 
-class wordle:
-    def __init__(self, user_id):
-        self.user_id = user_id
-        self.crystals = 0
+class WordleGame:
+    def __init__(self, user: User):
+        self.user = user
         self.wordle_daily_limit = 20
 
-    async def update(self):
-        await db.users.update_one(
-            {'user_id': self.user_id},
-            {'$set': {'info': pickle.dumps(self)}},
-            upsert=True
-        )
-
-    
+    async def update_user_crystals(self, crystals: int):
+        self.user.crystals += crystals
+        await self.user.update()
