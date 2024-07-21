4ffa86abd3e017da08f289d07d16bf24829903db
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-07-21 13:53:49 +0530

    Update wordle.py

--- YxH/Database/wordle.py
+++ YxH/Database/wordle.py
@@ -14,66 +14,51 @@ def today():
 async def add_game(user_id: int):
     user_id = str(user_id)
     x = await adb.find_one({"_": "_"})
-    if x:
-        dic = x['dic']
-    else:
-        dic = {}
-    if user_id in dic:
-        dic[user_id] = str(int(dic[user_id]) + 1)
-    else:
-        dic[user_id] = '1'
+    dic = x['dic'] if x else {}
+    dic[user_id] = str(int(dic.get(user_id, '0')) + 1)
     await adb.update_one({"_": "_"}, {"$set": {"dic": dic}}, upsert=True)
 
 async def get_wordle_dic():
     x = await adb.find_one({"_": "_"})
-    if x:
-        return x['dic']
-    return {}
+    return x['dic'] if x else {}
 
 async def add(user_id: int, guesses: int):
+    user_id = str(user_id)
     x = await cdb.find_one({"user_id": user_id})
-    if x:
-        lis = x['lis']
-    else:
-        lis = []
+    lis = x['lis'] if x else []
     lis.append(guesses)
     await cdb.update_one({"user_id": user_id}, {"$set": {"lis": lis}}, upsert=True)
 
 async def get_avg(user_id: int):
+    user_id = str(user_id)
     x = await cdb.find_one({"user_id": user_id})
-    if x:
-        lis = x['lis']
-    else:
-        lis = []
+    lis = x['lis'] if x else []
     total = sum(lis)
     count = len(lis)
     return total / count if count > 0 else 0
 
 async def incr_game(user_id: int):
+    user_id = str(user_id)
     td = today()
     x = await ldb.find_one({"user_id": user_id})
-    if x:
-        dic = x["dic"]
-        dic[td] = dic.get(td, 0) + 1
-    else:
-        dic = {td: 1}
+    dic = x["dic"] if x else {}
+    dic[td] = dic.get(td, 0) + 1
     await ldb.update_one({"user_id": user_id}, {"$set": {"dic": dic}}, upsert=True)
 
 async def get_today_games(user_id: int):
+    user_id = str(user_id)
     td = today()
     x = await ldb.find_one({"user_id": user_id})
-    if x:
-        dic = x["dic"]
-        return dic.get(td, 0)
-    return 0
+    dic = x["dic"] if x else {}
+    return dic.get(td, 0)
 
 async def get_all_games(user_id: int):
+    user_id = str(user_id)
     x = await ldb.find_one({"user_id": user_id})
-    if x:
-        return x["dic"]
-    return {}
+    return x["dic"] if x else {}
 
 async def add_crystal(user_id: int, crystals: int):
+    user_id = str(user_id)
     user_data = await udb.find_one({"user_id": user_id})
     if user_data:
         user_info = pickle.loads(user_data['info'])
