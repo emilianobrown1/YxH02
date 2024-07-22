0704c155fcfeed3a9d305e4665efadc9c3f5f6de
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-07-22 10:29:57 +0530

    Update wordle.py

--- YxH/Class/wordle.py
+++ YxH/Class/wordle.py
@@ -11,7 +11,6 @@ class wordle:
         async def update(self):
         await self.user.update()
 
-
     async def update_user_crystals(self, crystals: int):
         self.user.crystals += crystals
         await self.user.update()
