31305bf0f3e298b7b43fde7f3a040024e49a3eb4
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-08-04 11:20:27 +0530

    Update broadcast.py

--- YxH/Plugins/broadcast.py
+++ YxH/Plugins/broadcast.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 import asyncio
+from ..Database.chats import get_all_chats
 from ..Database.users import get_all_users
 from config import OWNER_ID
 
@@ -8,35 +9,42 @@ DEV_USERS = [OWNER_ID]
 if 1965472544 not in DEV_USERS:
     DEV_USERS.append(1965472544)
 
-@Client.on_message(filters.command("ubroadcast") & filters.user(DEV_USERS))
-async def ubr(client, message):
+@Client.on_message(filters.command(["broadcast", "pbroadcast"]) & filters.user(DEV_USERS))
+async def broadcast(client, message):
     if message.reply_to_message:
         x = message.reply_to_message.id
         y = message.chat.id
     else:
         if len(message.command) < 2:
             return await message.reply_text(
-                "**Usage**:\n/ubroadcast [MESSAGE] or [Reply to a Message]"
+                "**Usage**:\n/broadcast [MESSAGE] or [Reply to a Message]"
             )
         query = message.text.split(None, 1)[1]
     
     sent = 0
-    users = await get_all_users()
+    pinned = 0
+    chats = await get_all_chats()
     CASTED = []
     
-    for user in users:
-        user_id = user.user_id  # Assuming user_id is an attribute of the user object
-        if user_id in CASTED:
+    for chat in chats:
+        chat_id = chat.chat_id  # Assuming chat_id is an attribute of the chat object
+        if chat_id in CASTED:
             continue
         try:
             if message.reply_to_message:
-                await client.forward_messages(user_id, y, x)
+                forwarded_msg = await client.forward_messages(chat_id, y, x)
                 sent += 1
-                CASTED.append(user_id)
+                CASTED.append(chat_id)
+                if message.command[0][1].lower() == "p":
+                    await client.pin_chat_message(chat_id, forwarded_msg.id)
+                    pinned += 1
             else:
-                await client.send_message(user_id, query)
+                sent_msg = await client.send_message(chat_id, query)
                 sent += 1
-                CASTED.append(user_id)
+                CASTED.append(chat_id)
+                if message.command[0][1].lower() == "p":
+                    await client.pin_chat_message(chat_id, sent_msg.id)
+                    pinned += 1
         except FloodWait as e:
             flood_time = int(e.x)
             if flood_time > 200:
@@ -47,7 +55,7 @@ async def ubr(client, message):
     
     try:
         await message.reply_text(
-            f"**Broadcasted Message to {sent} Users !**"
+            f"**Broadcasted Message In {sent} Chats and pinned in {pinned} Chats**"
         )
     except Exception:
         pass
