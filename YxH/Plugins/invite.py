06c82961255f8914fcdaf2b34d7c1b37417328dd
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-08-08 21:18:25 +0530

    Update invite.py

--- YxH/Plugins/invite.py
+++ YxH/Plugins/invite.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters
 from ..Class import User
+from . import YxH, get_user
 
 @Client.on_message(filters.command("invite") & filters.private)
 async def invite(_, m):
