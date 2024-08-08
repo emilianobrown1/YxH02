cda8e4212797178565b402707717b8d1e869b5a0
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-08-08 13:29:41 +0530

    Update users.py

--- YxH/Database/users.py
+++ YxH/Database/users.py
@@ -1,5 +1,4 @@
 from . import db
-from YxH.Class.wordle import wordle
 import pickle
 
 db = db.users
@@ -9,7 +8,7 @@ async def get_user(user_id):
   if x:
     return pickle.loads(x['info'])
   return None
-  
+
 async def get_all_users():
   x = db.find()
   x = await x.to_list(length=None)
