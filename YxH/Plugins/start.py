5ebff2ef1453b69d809ee0cb0b210f796e902343
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-08-08 22:01:04 +0530

    Update start.py

--- YxH/Plugins/start.py
+++ YxH/Plugins/start.py
@@ -1,5 +1,4 @@
 from pyrogram import Client, filters
-from pyrogram.errors import UserAlreadyParticipant
 from ..Class import User
 from ..Utils.strings import start_text
 from ..Utils.markups import start_markup
@@ -7,7 +6,6 @@ from ..Database.users import get_user
 from ..Database.clan import get_clan
 from .clan import clan_info
 
-GROUP_CHAT_ID = -1002014537230  # Replace with your group's chat ID
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start(_, m):
@@ -25,22 +23,4 @@ async def start(_, m):
         u = User(m.from_user)
         await u.update()
 
-        # Award 100 crystals to the new user
-        u.crystals += 100
-        await u.update()  # Save the updated user data to the database
-
-        # Try to add the user to the group
-        try:
-            await _.add_chat_members(GROUP_CHAT_ID, [m.from_user.id])
-        except UserAlreadyParticipant:
-            pass  # User is already in the group
-
-    else:
-        # Handle rewards for users who have been invited
-        if user.invited_by:
-            inviter = await get_user(user.invited_by)
-            if inviter:
-                inviter.crystals += 50
-                await inviter.update()
-                user.invited_by = None  # Reset after rewarding
-                await user.update()
+        
