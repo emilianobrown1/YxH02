a59e7ffef1b9abdcf9a1f4d59c1d490f92bde98f
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-08-08 18:44:24 +0530

    Update user.py

--- YxH/Class/user.py
+++ YxH/Class/user.py
@@ -42,12 +42,6 @@ class User:
         self.max_gems = 5_000_000
         self.max_gold = 1_000_000_000_0
 
-    async def load_from_db(self):
-        data = await db.users.find_one({'user_id': self.user.id})
-        if data and 'info' in data:
-            db_info = pickle.loads(data['info'])
-            self.__dict__.update(db_info.__dict__)
-
     async def update_invite_link(self, link):
         self.invite_link = link
         await self.update()
