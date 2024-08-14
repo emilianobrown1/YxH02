ed905eb50480c720517151484c2eea61df286e72
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-08-15 02:34:33 +0530

    Update user.py

--- YxH/Class/user.py
+++ YxH/Class/user.py
@@ -52,7 +52,7 @@ class User:
         self.gems = min(self.gems, self.max_gems)
         self.gold = min(self.gold, self.max_gold)
         await db.users.update_one(
-        {'user_id': self.user},  # Use self.user directly as it's an integer
+        {'user_id': self.user.id},  
         {'$set': {'info': pickle.dumps(self)}},
         upsert=True
     )
