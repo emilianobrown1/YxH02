9a55de35761e563a38864bc1ab7499593416ce49
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-09-01 15:20:46 +0530

    Update block.py

--- YxH/Plugins/block.py
+++ YxH/Plugins/block.py
@@ -2,29 +2,19 @@ from pyrogram import Client, filters
 from ..Database.users import get_user
 from ..Class.user import User
 from config import SUDO_USERS, OWNER_ID
+import pickle
 
 @Client.on_message(filters.command("block") & filters.user(SUDO_USERS + [OWNER_ID]))
 async def block_user_command(client, message):
-    user_id = None
-
-    if message.reply_to_message:
-        user_id = message.reply_to_message.from_user.id
-    else:
-        if len(message.command) > 1:
-            user_id = int(message.command[1])
-
+    user_id = await get_user_id(message)
     if not user_id:
-        await message.reply("Please reply to a user or provide a user ID to block.")
+        await message.reply("Please reply to a user or provide a valid user ID to block.")
         return
 
-    user_data = await get_user(user_id)
-    if not user_data:
+    user = await get_user_object(user_id)
+    if not user:
         await message.reply("User not found. They need to start the bot first.")
         return
-
-    user = pickle.loads(user_data['info'])
-
-    print(f"Before blocking, user {user_id} blocked status: {user.blocked}")
 
     if user.blocked:
         await message.reply("User is already blocked.")
@@ -33,33 +23,19 @@ async def block_user_command(client, message):
     user.block_user()
     await user.update()
 
-    print(f"After blocking, user {user_id} blocked status: {user.blocked}")
-
     await message.reply(f"User {user_id} has been blocked.")
-
 
 @Client.on_message(filters.command("unblock") & filters.user(SUDO_USERS + [OWNER_ID]))
 async def unblock_user_command(client, message):
-    user_id = None
-
-    if message.reply_to_message:
-        user_id = message.reply_to_message.from_user.id
-    else:
-        if len(message.command) > 1:
-            user_id = int(message.command[1])
-
+    user_id = await get_user_id(message)
     if not user_id:
-        await message.reply("Please reply to a user or provide a user ID to unblock.")
+        await message.reply("Please reply to a user or provide a valid user ID to unblock.")
         return
 
-    user_data = await get_user(user_id)
-    if not user_data:
+    user = await get_user_object(user_id)
+    if not user:
         await message.reply("User not found.")
         return
-
-    user = pickle.loads(user_data['info'])
-
-    print(f"Before unblocking, user {user_id} blocked status: {user.blocked}")
 
     if not user.blocked:
         await message.reply("User is not blocked.")
@@ -68,6 +44,20 @@ async def unblock_user_command(client, message):
     user.unblock_user()
     await user.update()
 
-    print(f"After unblocking, user {user_id} blocked status: {user.blocked}")
-
     await message.reply(f"User {user_id} has been unblocked.")
+
+async def get_user_id(message):
+    if message.reply_to_message:
+        return message.reply_to_message.from_user.id
+    elif len(message.command) > 1:
+        try:
+            return int(message.command[1])
+        except ValueError:
+            return None
+    return None
+
+async def get_user_object(user_id):
+    user_data = await get_user(user_id)
+    if not user_data:
+        return None
+    return pickle.loads(user_data['info'])
