ce005f4e25dbd799d32617c6d6c71bb702bf8623
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-10-09 13:54:29 +0530

    Update swap.py

--- YxH/Plugins/swap.py
+++ YxH/Plugins/swap.py
@@ -4,53 +4,48 @@ from . import YxH
 from ..Class.user import User
 from ..Database.characters import get_anime_character_ids
 import time
-import random
-import pickle
 
 @Client.on_message(filters.command("swapx"))
-async def swapx(_, m, u):
-    user_id = m.from_user.id  # Assuming the user ID is the sender's ID
-    
-    # Check if the command is used on Wednesday
+async def swapx(_, m):
+    user_id = m.from_user.id  # Get the user ID of the sender
+
+    # Ensure the command is only used on Wednesday
     if time.strftime("%A").lower() != "wednesday":
         return await m.reply("This command can only be used on Wednesday.")
-    
-    # Retrieve the user's data
-    dic = await get_user(user_id)
-    str_user_id = str(user_id)
-    
-    # Check if the user has already performed 3 swaps
-    done = dic.get(str_user_id, 0)
-    if done >= 3:
-        return await m.reply('Maximum swaps reached for today!')
-    
+
+    # Retrieve the user's data from the database
+    user_data = await get_user(user_id)
+    if not user_data:
+        return await m.reply("User data not found.")
+
+    # Check if the user has performed 3 swaps today
+    swap_count = user_data.get("swap", 0)
+    if swap_count >= 3:
+        return await m.reply("Maximum swaps reached for today!")
+
     # Parse the old and new character IDs
     try:
         from_id = int(m.text.split()[1])
         to_id = int(m.text.split()[2])
     except (IndexError, ValueError):
         return await m.reply('Usage:\n\n`/swapx [old_id] [new_id]`')
-    
+
     # Check if the new character ID is valid
     total_characters = await get_anime_character_ids()
     if to_id > total_characters:
         return await m.reply("Invalid new character ID.")
-    
-    # Check if the user owns the old character
-    user_data = await get_user(user_id)
+
+    # Verify if the user owns the character to be swapped
     if str(from_id) not in user_data["characters"]:
-        return await m.reply(f'You do not have a character with ID `{from_id}`!')
-    
+        return await m.reply(f'You do not own the character with ID `{from_id}`!')
+
     # Perform the swap: remove the old character and add the new one
     user = User(user_id)
     await user.remove_character(from_id)
     await user.add_character(to_id)
-    
-    # Update the swap counter and save the updated data
-    dic[str_user_id] = done + 1
-    
-    # Save the updated dictionary to the database by pickling the updated info
-    user_data['info'] = pickle.dumps(dic)
-    await db.update_one({'user_id': user_id}, {'$set': {'info': user_data['info']}})
-    
+
+    # Increment the swap count and update the user data in the database
+    user_data["swap"] = swap_count + 1
+    await user.update()  # Save the updated user data using the User class
+
     await m.reply(f'Successfully swapped character {from_id} with {to_id}!')
