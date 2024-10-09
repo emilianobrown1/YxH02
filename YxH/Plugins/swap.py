fc975aa042dea45be353128bd0ea8f56f779762d
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-10-09 15:02:24 +0530

    Update swap.py

--- YxH/Plugins/swap.py
+++ YxH/Plugins/swap.py
@@ -1,25 +1,19 @@
 from pyrogram import Client, filters
 from ..Database.users import get_user
 from . import YxH
-from ..Class.user import User
 from ..Database.characters import get_anime_character_ids
 import time
 
 @Client.on_message(filters.command("swapx"))
-async def swapx(_, m):
-    user_id = m.from_user.id  # Get the user ID of the sender
+async def swapx(_, m, u):
+    user_id = u.user.id  # Get the user ID from the User instance
 
     # Ensure the command is only used on Wednesday
     if time.strftime("%A").lower() != "wednesday":
         return await m.reply("This command can only be used on Wednesday.")
 
-    # Retrieve the user's data from the database
-    user_data = await get_user(user_id)
-    if not user_data:
-        return await m.reply("User data not found.")
-
     # Check if the user has performed 3 swaps today
-    swap_count = user_data.swap.get("count", 0)  # Using the `swap` attribute
+    swap_count = u.swap.get("count", 0)  # Accessing the swap attribute from the User instance
     if swap_count >= 3:
         return await m.reply("Maximum swaps reached for today!")
 
@@ -36,16 +30,17 @@ async def swapx(_, m):
         return await m.reply("Invalid new character ID.")
 
     # Verify if the user owns the character to be swapped
-    if str(from_id) not in user_data.collection:
+    if str(from_id) not in u.collection:  # Ensure u.collection holds the character IDs
         return await m.reply(f'You do not own the character with ID `{from_id}`!')
 
-    # Perform the swap: remove the old character and add the new one
-    user = User(user_id)
-    await user.remove_character(from_id)
-    await user.add_character(to_id)
+    # Remove the old character from the user's collection
+    u.collection.remove(str(from_id))  # Assuming collection is a list; otherwise use `del u.collection[str(from_id)]`
+
+    # Add the new character to the user's collection
+    u.collection.append(str(to_id))  # Adding the new character ID
 
     # Increment the swap count and update the user data in the database
-    user_data.swap["count"] = swap_count + 1
-    await user.update()  # Save the updated user data using the User class
+    u.swap["count"] = swap_count + 1
+    await u.update()  # Save the updated user data using the User class
 
     await m.reply(f'Successfully swapped character {from_id} with {to_id}!')
