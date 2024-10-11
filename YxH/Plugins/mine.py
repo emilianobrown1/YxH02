ce15e7c746e862ef3a51a54cef10dccbe9d13bce
phil4242ad <142519189+phil4242ad@users.noreply.github.com>
2024-10-11 23:14:12 +0530

    Update mine.py

--- YxH/Plugins/mine.py
+++ YxH/Plugins/mine.py
@@ -33,7 +33,7 @@ async def mine(_, m, user):
         after = 60 - min
         return await m.reply(f"Mining limit reached, try again after `{after}` minutes.")
     user.mine[now] = val + 1
-    success = random.choices([True, False], weights=[95, 5], k=1)[0]
+    success = random.choice([True, False])
     
     percentage = random.choice(percentage_range)
     gold = int((inp * percentage) / 100)
