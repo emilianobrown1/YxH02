229d223443e6014768e36249d657f3acad91641f
Phil-coder140 <tashumansinghka175@gmail.com>
2024-11-23 21:09:04 +0530

    Update user.py

--- YxH/Class/user.py
+++ YxH/Class/user.py
@@ -38,6 +38,7 @@ class User:
         self.swap = {
             "count": 0  # Track the number of swaps
         }
+        self.barracks = 0
         self.inventory = {
             "Magic Key 🗝️": 0,
             "Magic Diamond 💎": 0,
