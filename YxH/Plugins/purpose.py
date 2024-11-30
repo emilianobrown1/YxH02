9b5800c16b6f85bb47f9869c2d52d528b86aa321
Phil-coder140 <tashumansinghka175@gmail.com>
2024-11-30 21:56:01 +0530

    Update purpose.py

--- YxH/Plugins/purpose.py
+++ YxH/Plugins/purpose.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from ..Database.couple import add_couple, rmv_couple, get_couple
 from ..class.user import User
-from ..class.couple import Couple
+from .class.couple import Couple
 
 @Client.on_message(filters.command("propose"))
 async def propose_command(client, message):
