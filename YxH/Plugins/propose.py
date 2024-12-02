9061216cc2769f6df3178e516c1cf83bd406d94d
Phil-coder140 <tashumansinghka175@gmail.com>
2024-12-02 13:44:37 +0530

    Update propose.py

--- YxH/Plugins/propose.py
+++ YxH/Plugins/propose.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from ..Database.couple import get_couple, get_top_couples, add_message_gems
+from ..Database.users import get_user
 from ..Class.user import User
 from ..Class.couple import Couple
 
@@ -125,13 +126,13 @@ async def top_couples_handler(client: Client, message: Message):
         user1_id = couple["user1"]
         user2_id = couple["user2"]
         
-        # Fetch User objects for user1 and user2
-        user1 = User(user1_id)
-        user2 = User(user2_id)
+        # Fetch User objects for user1 and user2 from the database
+        user1 = await get_user(user1_id)
+        user2 = await get_user(user2_id)
         
-        # Fetch the first name of both users (assuming User class has a 'user' attribute with first_name)
-        user1_name = user1.user.first_name if user1.user else f"User{user1_id}"
-        user2_name = user2.user.first_name if user2.user else f"User{user2_id}"
+        # Fetch the first name of both users (or use fallback if user data is unavailable)
+        user1_name = user1.first_name if user1 else f"User{user1_id}"
+        user2_name = user2.first_name if user2 else f"User{user2_id}"
         
         # Safely get 'message_gems', default to 0 if not found
         gems = couple.get("message_gems", 0)
