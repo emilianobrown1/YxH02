3ab1f707aa155bbe9c0ebf315c6eb06a0a244de3
Alpha <124826956+Alpha-Like@users.noreply.github.com>
2025-01-04 10:20:51 +0530

    Create fest_hour.py

---
+++ YxH/Database/fest_hour.py
@@ -0,0 +1,19 @@
+from . import db
+import random
+import datetime
+
+db = db.fest_hour
+
+def today():
+    return str(datetime.datetime.now()).split()[0]
+
+async def set_fest_hour() -> int:
+    random_hour = random.randint(0, 23)
+    await db.insert_one({'date': today(), 'fest_hour': random_hour})
+    return random_hour
+
+async def get_fest_hour() -> int:
+    x = await db.find({'date': today()})
+    if x:
+        return x['fest_hour']
+    return await set_fest_hour()
