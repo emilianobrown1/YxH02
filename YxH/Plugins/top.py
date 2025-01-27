339ca5cd62571b5aa2b0ccf5b16be61d20ac7cd5
Lol <142519189+Man-sukh@users.noreply.github.com>
2025-01-27 22:49:35 +0530

    Update top.py

--- YxH/Plugins/top.py
+++ YxH/Plugins/top.py
@@ -3,7 +3,6 @@ from ..Database.users import get_all_users
 from ..universal_decorator import YxH
 import heapq
 from . import YxH
-from ..load_attr import load_attr
 
 TOP_MINERS_IMAGE_PATH = "Images/mtop.jpg"
 TOP_COLLECTORS_IMAGE_PATH = "Images/top.jpg"
@@ -20,12 +19,9 @@ async def top(client, m, u):
     # Get the top 10 users based on the amount of gold they hold
     top10 = heapq.nlargest(10, users, key=key_func)
     txt = "Top Miners\n\n"
-    seen_users = set()
     for x, y in enumerate(top10):
-        if y.user_id not in seen_users:
-            user_info = await client.get_users(y.user_id)  # Dynamically fetch user info
-            txt += f"{x+1}. {user_info.first_name} ({y.user}) - {y.gold}\n"
-            seen_users.add(y.user)
+        user_info = await client.get_users(y.id)  # Dynamically fetch user info
+        txt += f"{x+1}. {user_info.first_name} ({y.id}) - {y.gold}\n"
     with open(TOP_MINERS_IMAGE_PATH, "rb") as image_file:
         await m.reply_photo(image_file, caption=txt)
 
@@ -40,12 +36,9 @@ async def ctop(client, m, u):
     # Get the top 10 users based on the collection size
     top10 = heapq.nlargest(10, users, key=c_func)
     txt = "Top Collectors\n\n"
-    seen_users = set()
     for x, y in enumerate(top10):
-        if y.user_id not in seen_users:
-            user_info = await client.get_users(y.user)  # Dynamically fetch user info
-            txt += f"{x+1}. {user_info.first_name} ({y.user}) - {len(y.collection)}\n"
-            seen_users.add(y.user_id)
+        user_info = await client.get_users(y.id)  # Dynamically fetch user info
+        txt += f"{x+1}. {user_info.first_name} ({y.id}) - {len(y.collection)}\n"
     with open(TOP_COLLECTORS_IMAGE_PATH, "rb") as image_file:
         await m.reply_photo(image_file, caption=txt)
 
@@ -60,11 +53,8 @@ async def crtop(client, m, u):
     # Get the top 10 users based on the amount of crystals they hold
     top10 = heapq.nlargest(10, users, key=cr_func)
     txt = "Top Crystal Holders\n\n"
-    seen_users = set()
     for x, y in enumerate(top10):
-        if y.user_id not in seen_users:
-            user_info = await client.get_users(y.user_id)  # Dynamically fetch user info
-            txt += f"{x+1}. {user_info.first_name} ({y.user}) - {y.crystals}\n"
-            seen_users.add(y.user)
+        user_info = await client.get_users(y.id)  # Dynamically fetch user info
+        txt += f"{x+1}. {user_info.first_name} ({y.id}) - {y.crystals}\n"
     with open(TOP_CRYSTAL_HOLDERS_IMAGE_PATH, "rb") as image_file:
         await m.reply_photo(image_file, caption=txt)
