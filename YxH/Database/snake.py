49d5240f0a2895215d04bea1f2d0884675e2deee
Lol <142519189+Man-sukh@users.noreply.github.com>
2025-01-31 13:26:34 +0530

    Update Database

---
+++ YxH/Database/snake.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from . import db
+
+snake_db = db.snake_games
+
+async def add_snake_game(winner_id, player_ids):
+    await snake_db.insert_one({
+        "winner": winner_id,
+        "players": player_ids,
+        "duration": datetime.now().timestamp() - start_time,
+        "timestamp": datetime.now()
+    })
