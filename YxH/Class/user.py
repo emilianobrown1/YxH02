f65477ea3c4317a4deaf77b5b9bbb89525e78f4d
Lol <142519189+Man-sukh@users.noreply.github.com>
2025-02-19 14:49:15 +0530

    Update user.py

--- YxH/Class/user.py
+++ YxH/Class/user.py
@@ -37,8 +37,11 @@ class User:
         self.buy_crystals = {}
         self.scramble = []
         self.troops = {"shinobi": 0, "wizard": 0, "sensei": 0}
-        self.powers = {"Hammer": 0, "ice": 0, "lightning": 0}
-        self.beasts = {"dragon": 0, "phoenix": 0, "tiger": 0}
+        self.powers = {"Darkness Shadow": 0, "Frost Snow": 0, "Thunder Storm": 0,
+            "Nature Ground": 0, "Flame Heat Inferno": 0, "Aqua Jet": 0}
+        self.beasts = {"Titanus Aegisorn": 0, "Glacelynx": 0, "Voltiscar": 0,
+            "Cerberus": 0, "Ignirax": 0, "Frostclaw": 0,
+            "Vilescale": 0, "Pyraxion": 0}
         self.swap = {
             "count": 0  # Track the number of swaps
         }
