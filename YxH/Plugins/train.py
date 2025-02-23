422a7131fa611dd4721835de9af0907e8d94f507
Lol <142519189+Man-sukh@users.noreply.github.com>
2025-02-23 17:32:57 +0530

    Update train.py

--- YxH/Plugins/train.py
+++ YxH/Plugins/train.py
@@ -2,7 +2,7 @@ e
 from pyrogram import Client, filters
 import asyncio
 from ..Class.user import User
-from .. import YxH
+from . import YxH
 
 @Client.on_message(filters.command(["shinobi", "wizard", "sensei"]))
 @YxH()
