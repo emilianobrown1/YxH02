0787566843a50e08dc32f2b08542bbb52d37805b
Lol <142519189+Man-sukh@users.noreply.github.com>
2025-02-24 14:35:12 +0530

    Update user.py

--- YxH/Class/user.py
+++ YxH/Class/user.py
@@ -78,6 +78,14 @@ class User:
         """Generic method to add crystals"""
         self.crystals += amount
         await self.update()
+
+    def __setstate__(self, state):
+        # Remove barracks-related attributes from the state before updating
+        state.pop('barracks_manager', None)
+        state.pop('troops', None)
+        state.pop('barracks', None)
+        # Update the instance's __dict__ with the cleaned state
+        self.__dict__.update(state)
             
 
     def is_blocked(self):
