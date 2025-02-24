2ca0c98f82de15455e83435dc2ac20169955ee03
Lol <142519189+Man-sukh@users.noreply.github.com>
2025-02-24 17:36:11 +0530

    Update barracks.py

--- YxH/Class/barracks.py
+++
@@ -1,7 +0,0 @@
-def __setstate__(self, state):
-        # Remove barracks-related attributes from the state before updating
-        state.pop('barracks_manager', None)
-        state.pop('troops', None)
-        state.pop('barracks', None)
-        # Update the instance's __dict__ with the cleaned state
-        self.__dict__.update(state)
