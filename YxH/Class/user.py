e67cc761d866736cc0200a88bb346e742da5c77a
Lol <142519189+Man-sukh@users.noreply.github.com>
2025-02-24 17:35:57 +0530

    Update user.py

--- YxH/Class/user.py
+++ YxH/Class/user.py
@@ -79,14 +79,6 @@ class User:
         self.crystals += amount
         await self.update()
 
-    def __setstate__(self, state):
-        # Remove barracks-related attributes from the state before updating
-        state.pop('barracks_manager', None)
-        state.pop('troops', None)
-        state.pop('barracks', None)
-        # Update the instance's __dict__ with the cleaned state
-        self.__dict__.update(state)
-            
 
     def is_blocked(self):
         return self.blocked
