9eff69c9cea6141fe802439ac87b7306e829d350
Lol <142519189+Man-sukh@users.noreply.github.com>
2025-02-28 02:44:04 +0530

    Update users.py

--- YxH/Database/users.py
+++ YxH/Database/users.py
@@ -1,6 +1,5 @@
 from . import db
 import pickle
-from .unpickler import safe_pickle_loads
 
 db = db.users
 
@@ -15,11 +14,3 @@ async def get_all_users():
   x = await x.to_list(length=None)
   return [pickle.loads(y["info"]) for y in x]
 
-
-
-async def get_user(user_id):
-    x = await db.find_one({'user_id': user_id})
-    return safe_pickle_loads(x['info']) if x else None
-
-async def get_all_users():
-    return [safe_pickle_loads(y["info"]) for y in await db.find().to_list(length=None)]
