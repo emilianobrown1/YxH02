15aafd342801413e7380eb26a1c1372cde0f1b36
Lol <142519189+Man-sukh@users.noreply.github.com>
2025-03-10 22:35:32 +0530

    Update catch.py

--- YxH/Plugins/catch.py
+++ YxH/Plugins/catch.py
@@ -73,7 +73,8 @@ async def beast_spawner(_, m):
         return
 
     if not hasattr(chat, "beast_status"):
-
+    chat.beast_status = None
+    
     if chat.beast_status:
         return  # A beast is already active in this chat
     
