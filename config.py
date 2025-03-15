077062dea0f9b3fad15a839c23ba447b5e5c479e
Lol <142519189+Man-sukh@users.noreply.github.com>
2025-03-15 20:39:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import getenv
 API_ID = int(getenv("API_ID", "28872999"))
 API_HASH = getenv("API_HASH", "7bab29feade5c8d36a5b47645eb769cb")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "6410981698:AAEIqbyK_2elx2wJUELVuRSOrxOA50eFV3Y")
+BOT_TOKEN = getenv("BOT_TOKEN", "6410981698:AAFwP2bIFz8i0EbSdVCK9XtpFQ8-x_O88kc")
   
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://phil:U2LIDcW5rxN2UMpX@cluster0.vvs2pmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
@@ -11,7 +11,7 @@ SUDO_USERS = getenv("SUDO_USERS",  "1086394021")
 OWNER_ID = int(getenv("OWNER_ID", "1965472544"))
 
 LOG_GROUP_ID = getenv("LOG_GROUP_ID", -1002461084481)
-ANIME_CHAR_CHANNEL_ID = -1002139511906
+ANIME_CHAR_CHANNEL_ID = -1002670254730
 
 # DO NOT CHANGE BELOW CODES.
 
