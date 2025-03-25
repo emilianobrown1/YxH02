80037fa42745498cbc923a1f97c5264f9ab2af80
Amolfeel <151847249+Amolfeel@users.noreply.github.com>
2025-03-25 10:38:04 +0530

    Create quest.py

---
+++ YxH/Database/quest.py
@@ -0,0 +1,15 @@
+from ..Database import db
+
+async def save_quest_data(user_id, quest_data):
+    await db.quests.update_one(
+        {"user_id": user_id},
+        {"$set": {"quest_data": quest_data}},
+        upsert=True
+    )
+
+async def get_quest_data(user_id):
+    user_data = await db.quests.find_one({"user_id": user_id})
+    return user_data.get("quest_data") if user_data else None
+
+async def delete_quest_data(user_id):
+    await db.quests.delete_one({"user_id": user_id})
