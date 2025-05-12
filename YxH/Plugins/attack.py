208c3a7e6c4e59947e3aef98e872c3ce6d470ad3
Lol <142519189+Man-sukh@users.noreply.github.com>
2025-05-12 21:36:19 +0530

    Update attack.py

--- YxH/Plugins/attack.py
+++ YxH/Plugins/attack.py
@@ -1,17 +1,15 @@
 from pyrogram import Client, filters
-from . import get_user, YxH
-from . import YxH, db
+from . import get_user, YxH, db
 import time
 import random
 
 @Client.on_message(filters.command('attack'))
 @YxH()
 async def attack(_, m, u):
-    # Must be in a clan
     if not u.clan_id:
         return await m.reply('You must be part of a clan to attack.')
-    
-    # Determine target: either via reply or via /attack <user_id>
+
+    # Determine target: via reply or /attack <user_id>
     target_user_id = None
     if m.reply_to_message:
         target_user_id = m.reply_to_message.from_user.id
@@ -21,23 +19,19 @@ async def attack(_, m, u):
             target_user_id = int(parts[1])
         else:
             return await m.reply('Usage: reply to a user with /attack or `/attack <user_id>`')
-    
-    # Fetch target user data
+
+    # Fetch target
     t = await get_user(target_user_id)
-    
-    # Cannot attack your own clan
+
     if u.clan_id == t.clan_id:
         return await m.reply("You cannot attack your clan mates.")
-    
-    # Require 10M gold to initiate attack
+
     if u.gold < 10_000_000:
         needed = 10_000_000 - u.gold
         return await m.reply(f'You need `{needed}` more gold to attack.')
-    
-    # Deduct attack fee
+
     u.gold -= 10_000_000
 
-    # Check and expire target’s shield
     if t.shield:
         duration, start = t.shield
         if time.time() - start > duration:
@@ -46,28 +40,34 @@ async def attack(_, m, u):
         await u.update()
         return await m.reply('Oops, target user has a shield equipped.')
 
-    # Prevent rapid repeated attacks
     if t.latest_defend and (time.time() - t.latest_defend) <= 10800:
         return await m.reply("Target was attacked recently; try again later.")
-    
-    # Calculate loot percentages
-    gold_per  = random.randint(5, 10)
-    gems_per  = random.randint(5, 10)
-    gold_val  = int(t.gold  * gold_per / 100)
-    gems_val  = int(t.gems  * gems_per / 100)
-    
-    # Transfer loot
-    t.gold   -= gold_val
-    t.gems   -= gems_val
-    u.gold   += gold_val
-    u.gems   += gems_val
+
+    gold_per = random.randint(5, 10)
+    gems_per = random.randint(5, 10)
+    gold_val = int(t.gold * gold_per / 100)
+    gems_val = int(t.gems * gems_per / 100)
+
+    t.gold -= gold_val
+    t.gems -= gems_val
+    u.gold += gold_val
+    u.gems += gems_val
     t.latest_defend = time.time()
-    
-    # Persist changes
+
+    # Save both users
     await u.update()
     await t.update()
-    
-    # Notify attacker and target
+
+    # Track attack in db.attacks for leaderboard
+    user_id = m.from_user.id
+    name = m.from_user.first_name
+    await db.attacks.update_one(
+        {"user_id": user_id},
+        {"$set": {"name": name}, "$inc": {"attack": 1}},
+        upsert=True
+    )
+
+    # Notify both users
     await m.reply(
         f'You\'ve looted `{gold_val}` Gold and `{gems_val}` Gems from **{t.user.first_name}**.'
     )
@@ -76,23 +76,3 @@ async def attack(_, m, u):
         f'You got attacked by **{u.user.first_name}**\n\n'
         f'Lost `{gold_val}` Gold and `{gems_val}` Gems.'
     )
-
-
-
-@Client.on_message(filters.command("attack"))
-@YxH()
-async def attack_handler(_, m, u):
-    user_id = m.from_user.id
-    name = m.from_user.first_name
-
-    # Save or update attack data
-    await db.attacks.update_one(
-        {"user_id": user_id},
-        {
-            "$set": {"name": name},
-            "$inc": {"attack": 1}
-        },
-        upsert=True
-    )
-
-    await m.reply("Attack executed! Your attack count has increased.")
