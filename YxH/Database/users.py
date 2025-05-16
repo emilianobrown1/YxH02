d23987fd37c64006ab554dcb169e117cea3eb1ad
Lol <142519189+Man-sukh@users.noreply.github.com>
2025-05-16 12:49:44 +0530

    Update users.py

--- YxH/Database/users.py
+++ YxH/Database/users.py
@@ -14,11 +14,3 @@ async def get_all_users():
   x = await x.to_list(length=None)
   return [pickle.loads(y["info"]) for y in x]
 
-async def update_user(user_id, data):
-    """Update user data in the database"""
-    await db.update_one(
-        {'user_id': user_id},
-        {'$set': {'info': data}},
-        upsert=True
-    )
-
