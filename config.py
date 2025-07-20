86aa8d9a2919cbf8c371f53aa38f51173bb53d9d
Lol <142519189+Man-sukh@users.noreply.github.com>
2025-07-20 12:00:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "6410981698:AAFwP2bIFz8i0EbSdVCK9XtpFQ8-x_O88kc"
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://phil:U2LIDcW5rxN2UMpX@cluster0.vvs2pmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 SUDO_USERS = getenv("SUDO_USERS",  "1086394021")
-OWNER_ID = int(getenv("OWNER_ID", "1965472544"))
+OWNER_ID = int(getenv("OWNER_ID", "5969161772"))
 
 LOG_GROUP_ID = getenv("LOG_GROUP_ID", -1002461084481)
 ANIME_CHAR_CHANNEL_ID = -1002670254730
